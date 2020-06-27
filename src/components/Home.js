c7682b23cf668dd720a2d372e1690c18a45ef804
Ayan <ayan.chowdhury2050@gmail.com>
2020-06-27 13:42:04 +0530

    signup-signin using react

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+export default function Home() {
+  return (
+    <Fragment>
+      <div>
+        <p className="has-text-centered">Aws Cognito React
+        </p>
+      </div>
+    </Fragment>
+  )
+}
